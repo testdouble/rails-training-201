fc198cb771f87f7fa2950673b30a32b07da78ad5
Kevin Baribeau <kevin.baribeau@gmail.com>
2020-07-12 17:06:03 -0600

    Revert "Factories work"
    We dont' want this repo to contain factories.
    
    The plan is to start students with fixtures and have them figure out
    factories on their own.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,8 +10,5 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  include FactoryBot::Syntax::Methods
-  FactoryBot.reload
-
   # Add more helper methods to be used by all tests here...
 end
