0e1ba9358fee94b6e925a957996c09ac1bd2109c
Ilana Corson <ilana.corson@gmail.com>
2020-08-03 15:59:58 -0700

    Update director_test.rb

--- test/models/director_test.rb
+++ test/models/director_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 class DirectorTest < ActiveSupport::TestCase
   test "director has associated movies" do
     bong_joon_ho = directors(:bong_joon_ho)
-    assert_equal 1, director.movies.count
+    assert_equal 1, bong_joon_ho.movies.count
   end
 end
